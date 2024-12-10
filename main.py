8a83b7c74907c6d475d0ecaffe6af35b98b9fcbb
AlexTLDR <alex.badragan@protonmail.com>
2024-12-10 13:01:44 +0100

    listed all the letters occurence

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     word_count = count_words(file_contents)
-    print(f"The total number of words is {word_count}")
-    char_count = count_characters(file_contents)
-    print(char_count)
 
+    char_count = count_characters(file_contents)
+
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
+
+    dictionaries = []
+    for char, count in char_count.items():
+        dictionaries.append({"char": char, "count": count})
+    sorted_chars = sorted(dictionaries, key=sort_on, reverse=True)
+    for sort in sorted_chars:
+        char = sort["char"]
+        count = sort["count"]
+        if char.isalpha():
+            print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
 
 
 def count_words(text):
@@ -21,5 +34,8 @@ def count_characters(text):
         else:
             characters[lowered] = 1
     return characters
+
+def sort_on(dictionary):
+    return dictionary["count"]
 
 main()
